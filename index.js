4ab803fe61109a49d5d2a77422b2b8748d03286e
Mischka Booley <mischka@gearbox.com.au>
2022-06-09 11:47:25 +1000

    commit tree b64d778a919383ff5893f6c94c56ebe5bee175d8

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+document.body.appendChild(newHeader);
+newHeader.textContent = "YOUR-NAME is the champion";
